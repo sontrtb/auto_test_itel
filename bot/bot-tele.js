33265c891198ebffaea6170cc833e76111035f3a
Phạm Hồng Sơn <sontrtb@gmail.com>
2025-07-23 08:57:20 +0700

    update: delete proxy

--- bot/bot-tele.js
+++ bot/bot-tele.js
@@ -3,15 +3,15 @@ import axios from 'axios'
 const token = '7624395023:AAHTS7nZuQhqarknD-blJ6OIKOr4oKNYy1g';
 const chatId = '-4623547189';
 
-const proxyConfig = {
-    host: '13.212.91.168',
-    port: 3128,
-    auth: {
-        username: 'teleproxy',
-        password: 'uZfmvdzQLMfe2dRgcKhAn5CmM5f9foQz',
-    },
-    protocol: 'http'
-};
+// const proxyConfig = {
+//     host: '13.212.91.168',
+//     port: 3128,
+//     auth: {
+//         username: 'teleproxy',
+//         password: 'uZfmvdzQLMfe2dRgcKhAn5CmM5f9foQz',
+//     },
+//     protocol: 'http'
+// };
 
 async function sendMess(mess) {
     try {
@@ -19,7 +19,7 @@ async function sendMess(mess) {
             chat_id: chatId,
             text: mess,
         }, {
-            proxy: proxyConfig
+            // proxy: proxyConfig
         });
 
         console.log('✅ Gửi tin nhắn thành công');
